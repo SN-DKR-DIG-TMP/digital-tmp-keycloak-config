b4ad36b50dc4f7f062c2d7d87de95f815b7f131d
Abdou-mbengue <abdou.mbengue.external@atos.net>
2025-07-10 15:45:38 +0000

    Update UserDtoModel.java

--- src/main/java/net/atos/digital/keycloak/models/UserDtoModel.java
+++ src/main/java/net/atos/digital/keycloak/models/UserDtoModel.java
@@ -3,6 +3,25 @@ package net.atos.digital.keycloak.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class UserDtoModel {
+
+    String userKeycloakId;
+
+    Long userId;
+
+    boolean enable;
+}
+package net.atos.digital.keycloak.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
